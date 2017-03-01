f2f73aed00746cc95a40c09f0613c80b044438dc
Eugene <eugene.gubar@gmail.com>
2017-03-02 01:00:39 +0200

    Add realize component for custom tag app-import

--- import/parts/app-import.js
+++ import/parts/app-import.js
@@ -4,6 +4,19 @@
 // =============================================================================
 (function () {
 
-    
+    // Create definition and prototyping
+    var appImportComponentProto = Object.create(HTMLElement.prototype);
+
+    // Call the callback function, clone and paste template #appImport content in custom element (<app-import></app-import>)
+    appImportComponentProto.createdCallback = function () {
+        var template = document.querySelector('#appImport');
+
+        this.appendChild(document.importNode(template.contant, true));
+    };
+
+    // Register and init element
+    document.registerElement('app-import', {
+        prototype: appImportComponentProto
+    });
 
 })();
